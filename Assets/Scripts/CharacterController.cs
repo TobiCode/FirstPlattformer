3c631b5b9fa30769e649a225260a8b4b66e9c5c1
TobiCode <tobias.w.weber@gmail.com>
2018-11-26 23:16:49 +0100

    Add all the code, prefabs, animations and picture data (including own created assets)

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterController : MonoBehaviour {
+
+    public float runSpeed;
+    public float moveSpeed;
+    public float jumpHeight;
+    private Animator anim;                  // The animator that controls the characters animations
+    public DialogueTrigger dialogueTrigger;
+    private bool talkedToCat;
+    public bool freeze;
+
+    private SpriteRenderer spriteRenderer;
+    private bool grounded;
+
+    //Life
+    private bool isDead;
+    public int lives;
+    public Image live1;
+    public Image live2;
+    public Image live3;
+    private float lastDamage;
+    public float againDamageTime;
+
+    public CatController catContoller;
+
+    //UI GameOver
+    public GameObject GameOver;
+
+   
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        lives = 3;
+        isDead = false;
+        talkedToCat = false;
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        freeze = false;
+    }
+
+    
+        //This method is called when the character collides with a collider (could be a platform).
+	void OnCollisionEnter2D(Collision2D hit)
+	{
+		//grounded = true;
+    
+	}
+
+   
+     
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(lives < 1)
+        {
+            isDead = true;
+         
+        }
+
+        if(isDead == true)
+        {
+            //Destroy(gameObject);
+            //reload Scene
+            //Is Working but make a Nice Scrren with failed etc
+            //GameManagerScript.RestartLevel();
+
+            //Show GameOverScreen 
+            freeze = true;
+            //GameOver.enabled = true;
+            GameOver.SetActive(true);
+         }
+
+        
+
+
+        if (!freeze)
+        {
+            //update lastTimeDamage
+            lastDamage += Time.deltaTime;
+            //Jump
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (grounded == true)
+                {
+                    grounded = false;
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+                    //We trigger the Jump animation state
+                    anim.SetBool("jumping", true);
+
+                }
+            }
+
+            //First check if shift is also clicked -> to run instead of walking
+            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(runSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                if (grounded == true)
+                {
+                    anim.SetBool("jumping", false);
+                    anim.SetFloat("speedRunning", 1);
+                }
+                if (spriteRenderer.flipX == true)
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+
+            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-runSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                anim.SetFloat("speed", 1);
+                if (grounded == true)
+                {
+                    anim.SetBool("jumping", false);
+                    anim.SetFloat("speedRunning", 1);
+                }
+                if (spriteRenderer.flipX == false)
+                {
+                    spriteRenderer.flipX = true;
+                }
+
+            }
+
+            else if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                if (grounded == true)
+                {
+                    anim.SetBool("jumping", false);
+                    anim.SetFloat("speed", 1);
+                }
+                if (spriteRenderer.flipX == true)
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+                anim.SetFloat("speed", 1);
+                if (grounded == true)
+                {
+                    anim.SetBool("jumping", false);
+                }
+                if (spriteRenderer.flipX == false)
+                {
+                    spriteRenderer.flipX = true;
+                }
+
+
+            }
+
+            else
+            {
+                anim.SetFloat("speed", 0);
+                anim.SetFloat("speedRunning", 0);
+                if (grounded == true)
+                {
+                    anim.SetBool("jumping", false);
+                }
+            }
+        }
+
+    }
+
+    //Check if the player is on the Ground
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.ToString() == "Ground")
+        {
+            grounded = true;
+        }
+
+        if (collision.gameObject.tag.ToString() == "Cat")
+        {
+            if (!talkedToCat)
+            {
+                freeze = true;
+                dialogueTrigger.TriggerDialogue();
+                talkedToCat = true;
+
+                //Activate CatController
+                catContoller.enabled = true;
+            }
+        }
+       
+       
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.ToString() == "Ground")
+        {
+            grounded = true;
+        }
+        //loose a live
+        if (collision.gameObject.tag.ToString() == "Obstacle")
+        {
+            if(lastDamage > againDamageTime)
+            {
+                lastDamage = 0;
+                looseLive();
+            }
+           
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag.ToString() == "Ground")
+        {
+            grounded = false;
+        }
+    }
+
+    public void looseLive()
+    {
+        
+        if(live1.enabled == true && live2.enabled == true && live3.enabled == true)
+        {
+            live3.enabled = false;
+        }
+        else if(live1.enabled == true && live2.enabled == true)
+        {
+            live2.enabled = false;
+        }
+        else if (live1.enabled == true)
+        {
+            live1.enabled = false;
+        }
+        else
+        {
+            live1.enabled = false;
+            live2.enabled = false;
+            live3.enabled = false;
+        }
+        lives = lives - 1;
+    }
+}
+
---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour {
+
+    private float PosX1;
+    private float PosY1;
+    public float PosX2;
+    public float PosY2;
+
+    private bool moveToPos2;
+    private bool moveToPos1;
+
+    public float movSpeed;
+
+
+    // Use this for initialization
+    void Start () {
+        PosX1 = gameObject.transform.position.x;
+        PosY1 = gameObject.transform.position.y;
+
+        moveToPos2 = true;
+        moveToPos1 = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (moveToPos2 == true)
+        {
+            //moove to Pos2
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if(gameObject.transform.position.x > PosX2)
+            {
+                moveToPos2 = false;
+                moveToPos1 = true;
+            }
+        }
+        if(moveToPos1 == true)
+        {
+            //moove to Pos1
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-movSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            if (gameObject.transform.position.x < PosX1)
+            {
+                moveToPos2 = true;
+                moveToPos1 = false;
+            }
+
+        }
+		
+    }
+}
